59432438469e78f5995f9a78a316776adcfe859e
Kontilenia-M Kotsifakou <47034534+Kontilenia@users.noreply.github.com>
2021-01-16 22:47:00 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -10,11 +10,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         with conn:
             print('Connected by', addr)
             while True:
-                data = conn.recv(19)
+                data = conn.recv(1000)
+                #Not in use
                 if not data or data==b'stop':
                     conn.close()
                     break
+                #In use
                 else:
-                    print (data.decode())
+                    clientinput = str(data.decode())
+                    print (clientinput)
                     conn.sendall(str.encode('thank you'))
                     print ("---------------")
+                    list1 = clientinput.split(",")
+                    print(list1)
+                    list2 = list1[1:]
+                    i = list2.index("@")
+                    columns = list2[:i]
+                    remain = list2[(i + 1):]
+                    print(columns)
+                    if list1[0]=='1' :
+                        print('case1')
+                    elif list1[0]=='2' :
+                        print('case2')
+                        i = remain.index("@")
+                        table = remain[:i]
+                        condition = remain[(i + 1):]
+                        print(table)
+                        print(condition)
