76d881a2c7269cfe6e18d0dc83dc1ce06683aa59
Sima <46862491+TheNewOwner@users.noreply.github.com>
2021-01-23 21:25:07 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
+#Importing socket to use our sockets and pickle to send objects through sockets
 import socket, pickle
+#Importing table to use the table function so we can show our table
 from table import Table
+#Importing regex to check correct user input
 import re
+#Reserved SQL words
 reserved = ['from', 'where', 'select']
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 8181       # The port used by the server
+#The server's hostname or IP address
+HOST = '127.0.0.1'
+#The port used by the server
+PORT = 8181
 
+#Creating a new socket object
 s = socket.socket()
+#Connect to the server with our given ip and port
 s.connect((HOST, PORT))
 while True:
     #Variable used for checking if the user input complies with the conditions
@@ -13,7 +21,9 @@ while True:
     print('Give an SQL Command or the word #stop# to stop the proccess')
     while(not correct):
         uinput = input()
+        #Make all the letters lower case for easier
         uinput = uinput.lower()
+        #Stop input stops the client connection
         if(uinput == 'stop'):
             break
         #Checking if the input matches the regex used for the sql command
@@ -21,10 +31,12 @@ while True:
         result = re.match(pattern, uinput)
         #If it does check if there are any reserved words in the command
         if result:
+            #Removing the whitespaces and unnecessary comas and putting all the words in a list by splitting them by the necessary comas
             x = uinput.replace(' ', ',')
             x = x.replace(',,', ',')
             x = x.replace(',,', ',')
             words = x.split(',')
+            #Checking if any of the words in the list belongs in the reserved list
             wrongex = False;
             for r in reserved:
                 count = 0
@@ -33,8 +45,10 @@ while True:
                         count +=1
                 if (count > 1):
                     wrongex = True
+            #If there is, the command is wrong
             if (wrongex):
                 print('Wrong Command Please try again!')
+            #Else it's correct
             else:
                 print('Correct')
                 correct = True
@@ -45,13 +59,17 @@ while True:
     if(uinput != 'stop'):
         tosend = ''
         existswhere = False
+        #If the word where is on the list, we have the case 2 which means existswhere gets the bool value True. If not we have case1.
         for word in words:
             if (word == 'where'):
                 existswhere = True
+        #cas2
         if existswhere:
             tosend += '2,'
+        #case1
         else:
             tosend += '1,'
+        #Changing the rest of the keywords to the selected identifier
         for word in words:
             if (word == 'select'):
                 pass
--- server.py
+++ server.py
@@ -1,24 +1,36 @@
+#Importing socket to use our sockets and pickle to send objects through sockets
 import socket, pickle
+#Importing database to use the respective functions the client sends to access the database.
 from database import Database
+#Importing sys to handle errors
 import sys
+#The port the server uses.
 PORT = 8181
 
-#Load db import Database
+#Asking the server user for the database name and loading it
 print('Give me the database name:')
 db_name = input()
 db = Database(db_name,load=True)
+#Create a socket object with the name s
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #Bind the port 8181 to the socket s
     s.bind(('', PORT))
     print ('socket binded to %s' %(PORT))
+    #Make the socket listen to max 5 connections
     s.listen(5)
+    #Keep it working
     while True:
+        #When a connection comes, accept it and name it conn
         conn, addr = s.accept()
         with conn:
             print('Connected by', addr)
+            #Keep receiving data until stopped
             while True:
+                #Receive the first 1000 bytes
                 data = conn.recv(1000)
                 #Check if the client wants to stop the connection
                 if not data or data==b'stop':
+                    #Pickle and send a message back
                     message_string = pickle.dumps('Thank you for accessing our database')
                     conn.sendall(message_string)
                     print('Closing the connection')
@@ -29,7 +41,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     clientinput = str(data.decode())
                     print (clientinput)
                     print ('---------------')
-                    #We split the client input for easier checking
+                    #We split the client input for easier checking by using the unique identifier
                     list1 = clientinput.split(',')
                     list2 = list1[1:]
                     i = list2.index('@')
@@ -42,17 +54,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         print('case1')
                         #Try to find the client requested table from the database
                         try:
+                            #Get the table and then send it after pickling it
                             utable = db.select(remain[0],columns, return_object = True)
                             table_string = pickle.dumps(utable)
                             conn.sendall(table_string)
+                            #Message of confirmation
                             print("The table has been sent")
                         #If it's not found send the key error to the client
                         except:
+                            #Get the error, pickle it and send it
                             e = sys.exc_info()[1]
                             print(e)
                             db.unlock_table('classroom')
                             error_string = pickle.dumps(e)
                             conn.sendall(error_string)
+                            #Message of confirmation
                             print("The error message has been sent")
                     elif list1[0]=='2':
                         print('case2')
@@ -61,15 +77,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         condition = remain[(i + 1):]
                         #Try to find the client requested table from the database
                         try:
+                            #Get the table and then send it after pickling it
                             utable = db.select(table[0],columns,condition[0],return_object = True)
                             table_string = pickle.dumps(utable)
                             conn.sendall(table_string)
+                            #Message of confirmation
                             print("The table has been sent")
                         #If it's not found send the key error to the client
                         except:
+                            #Get the error, pickle it and send it
                             e = sys.exc_info()[1]
                             print(e)
                             db.unlock_table('classroom')
                             error_string = pickle.dumps(e)
                             conn.sendall(error_string)
+                            #Message of confirmation
                             print("The error message has been sent")
